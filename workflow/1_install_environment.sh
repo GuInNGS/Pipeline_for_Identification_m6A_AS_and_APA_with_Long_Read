5282c032aa9fcbdc8fec0a2d461031b3365e737e
NGS_Gu <96377538+GuInNGS@users.noreply.github.com>
2022-02-26 08:14:59 +0000

    Update 1_install_environment.sh

--- workflow/1_install_environment.sh
+++ workflow/1_install_environment.sh
@@ -41,8 +41,6 @@ echo '---------------------------------'
 
 wget http://forestry.fafu.edu.cn/tool/PRAPI/prapi_env.yaml
 conda env create -f prapi_env.yaml 
-conda activate prapi_env 
-pip install -i https://pypi.anaconda.org/gaoyubang/simple splicegrapher 
 
 echo '---------------------------------'
 echo "[`date`] prapi INSTALL COMPLETE"
