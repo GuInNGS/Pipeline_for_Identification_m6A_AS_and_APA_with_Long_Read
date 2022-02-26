3de5b3eb96c155bcc88bc901543dc055acf51fe0
NGS_Gu <96377538+GuInNGS@users.noreply.github.com>
2022-02-26 06:40:48 +0000

    Update 1_install_environment.sh

--- workflow/1_install_environment.sh
+++ workflow/1_install_environment.sh
@@ -8,20 +8,36 @@ eval "$(conda shell.bash hook)"
 # pip install ont-fast5-api   #https://pypi.org/project/ont-fast5-api/
 # or user can install tombo by the following command
 
+echo '---------------------------------'
+echo "[`date`] tombo INSTALL BEGIN"
+echo '---------------------------------'
+
 conda env create -n tombo_env -f tombo.yaml
+
 echo '---------------------------------'
 echo "[`date`] tombo INSTALL COMPLETE"
+echo '---------------------------------'
+
 
 
 ### install nanom6A
 # Download the source package in https://drive.google.com/drive/folders/1Dodt6uJC7lBihSNgT3Mexzpl_uqBagu0?usp=sharing
 
+echo '---------------------------------'
+echo "[`date`] nanom6A INSTALL BEGIN"
+echo '---------------------------------'
+
 conda env create -n nanom6A_env -f nanom6a.yaml
+
 echo '---------------------------------'
 echo "[`date`] nanom6A INSTALL COMPLETE"
-
+echo '---------------------------------'
 
 ### install prapi
+
+echo '---------------------------------'
+echo "[`date`] prapi INSTALL COMPLETE"
+echo '---------------------------------'
 
 wget http://forestry.fafu.edu.cn/tool/PRAPI/prapi_env.yaml
 conda env create -f prapi_env.yaml 
@@ -30,3 +46,4 @@ pip install -i https://pypi.anaconda.org/gaoyubang/simple splicegrapher
 
 echo '---------------------------------'
 echo "[`date`] prapi INSTALL COMPLETE"
+echo '---------------------------------'
